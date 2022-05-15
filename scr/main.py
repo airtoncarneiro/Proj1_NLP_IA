7fcb2ce642b45f79c11e6037b3469ec85adb93c7
Airton Carneiro <airtoncarnero@gmail.com>
2022-05-15 14:24:53 -0300

    Entrega do projeto v0.1

--- scr/main.py
+++ scr/main.py
@@ -15,9 +15,8 @@ doc = stannize.get_words_stannizeds(my_list)
 
 # informa a quantidade de pontos centrais
 # que também será o top n palavras
-qtd_pontos_centrais = int =  3
+qtd_pontos_centrais = int =  5
 topn_words_table, words_proximities = \
     metrics.get_metrics_of_text(my_list, qtd_pontos_centrais)
 
 PrintWordCloud(topn_words_table, words_proximities, qtd_pontos_centrais)
-print(1)
