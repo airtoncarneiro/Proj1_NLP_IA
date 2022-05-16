21f517f13527827906df5943f1f91586e0781404
Airton Carneiro <airtoncarnero@gmail.com>
2022-05-16 11:20:34 -0300

    Alteração no retorno da staniza para voltar uma lista de stans

--- scr/main.py
+++ scr/main.py
@@ -3,14 +3,16 @@ import stannize
 import metrics
 from print_word_cloud import PrintWordCloud
 
+# Lê os arquivos e faz um pré-processamento
 my_list = preprocess_file.get_preprocessed_files()
-
-doc = stannize.get_words_stannizeds(my_list)
+# staniza a lista de palavras
+my_list = stannize.get_words_stannizeds(my_list)
 
 # informa a quantidade de pontos centrais
 # que também será o top n palavras
 qtd_pontos_centrais = int =  5
 topn_words_table, words_proximities = \
     metrics.get_metrics_of_text(my_list, qtd_pontos_centrais)
-
+    
+# mostra o gráfico
 PrintWordCloud(topn_words_table, words_proximities, qtd_pontos_centrais)
--- scr/stannize.py
+++ scr/stannize.py
@@ -3,19 +3,25 @@ import stanza
 def get_words_stannizeds(inner_lists):
     '''
     '''
-    nlp = stanza.Pipeline(lang='pt', processors='tokenize, lemma')
-    stanzed_list = []
+    nlp = stanza.Pipeline(lang='pt', processors='tokenize,lemma')
+    # stanzed_list = []
+    docs_in_lemman = []
     for a_list in inner_lists:
         joined_words = " ".join([' '.join(a_list)])
         doc = nlp(joined_words)
 
-        stanzed_list.append([
-            {'id':word.id, 
-             'word':word.text,
-             'lemma':word.lemma} \
+        # stanzed_list.append([
+        #     {'id':word.id, 
+        #      'word':word.text,
+        #      'lemma':word.lemma} \
+        #          for sent in doc.sentences \
+        #             for word in sent.words])
+        
+        docs_in_lemman.append([
+            word.lemma \
                  for sent in doc.sentences \
                     for word in sent.words])
 
 
-    return stanzed_list
+    return docs_in_lemman
     
