23d455c7e95d1d7b227b4ff58ed77278cf591b8c
Airton Carneiro <airtoncarnero@gmail.com>
2022-05-16 13:18:18 -0300

    Ainda inclusão de docstrings

--- scr/process_text.py
+++ scr/process_text.py
@@ -6,6 +6,13 @@ import re
 
 class Process_Text():
     def __init__(self, contents:list):
+        '''
+        Classe responsável pelo pré-processamento do texto.
+
+        Parameter:
+            contents (list) Uma lista com o conteúdo dos documentos.
+        '''
+
         original_text = ' '.join([str(text) for text in contents])
         lower = self._lower_case(original_text)
         noise_removed = self._remove_noises(lower)
@@ -17,12 +24,14 @@ class Process_Text():
 
     def _lower_case(self, text:str):
         '''
+        Transforma o texto para lower case.
         '''
 
         return text.lower()
 
     def _remove_noises(self, text):
         '''
+        Remove os ruídos (caracteres de pontuações, duplo espaços, etc).
         '''
 
         regex = '\n'
@@ -35,21 +44,26 @@ class Process_Text():
     
     def _remove_accents(self, text):
         '''
+        Remove os acentos das palavras.
         '''
 
         return unidecode.unidecode(text)
 
     def _tokenize(self, text):
         '''
+        Tokeniza o texto.
         '''
 
         return tokenize.word_tokenize(text, language='portuguese')
 
     def _stop_words(self, words):
         '''
+        Remove as palavras consideradas como Stop Words.
         '''
+        custom = ['i', 'se', 'ate', 'nao', 'tal']
 
-        stop_words = set(stopwords.words('portuguese') + list(punctuation))
+        stop_words = set(stopwords.words('portuguese') + \
+            list(punctuation) + custom)
         words_withous_sw = [word for word in words if word not in stop_words]
 
         return words_withous_sw
